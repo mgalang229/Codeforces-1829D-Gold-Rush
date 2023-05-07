ec89a824c443472916eff2f4abe33436b2cc4824
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-05-07 13:42:02 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,127 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static boolean possible;
+	
+	public static void main(String[] args) {
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int n = fs.nextInt(), m = fs.nextInt();
+			if (m == n) {
+				System.out.println("YES");
+				continue;
+			}
+			if (m > n || n % 3 != 0) {
+				System.out.println("NO");
+				continue;
+			}
+			possible = false;
+			recurse(n / 3, n - n / 3, m);
+			System.out.println(possible ? "YES" : "NO");
+		}
+		out.close();
+	}
+	
+	static void recurse(int x, int y, int m) {
+//		System.out.println(x + " " + y);
+		if (x == m || y == m) {
+			possible = true;
+			return;
+		}
+		if (x % 3 == 0) {
+			recurse(x / 3, x - x / 3, m);
+		}
+		if (y % 3 == 0) {
+			recurse(y / 3, y - y / 3, m);
+		}
+		return;
+	}
+	
+	static final Random rnd = new Random();
+	static void shuffleSort(int[] a) { //change this
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int newInd = rnd.nextInt(n);
+			int temp = a[newInd]; //change this
+			a[newInd] = a[i];
+			a[i] = temp;
+		}
+		Arrays.sort(a);
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextLong();
+			}
+			return a;
+		}
+		
+		double[] readDoubleArray(int n) {
+			double[] a = new double[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextDouble();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				if (st.hasMoreTokens()) {
+					str = st.nextToken("\n");
+				} else {
+					str = br.readLine();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
